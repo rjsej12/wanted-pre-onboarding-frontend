344bbcafcaa6f88f417e3604d6308efd3ce2abab
rjsej12 <rjsej12@naver.com>
2023-04-04 02:25:27 +0900

    feat: GlobalStyle 생성 및 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Global } from '@emotion/react';
 import React from 'react';
+import { globalStyles } from './commons/globalStyles';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Global styles={globalStyles} />
+    </div>
+  );
 }
 
 export default App;
